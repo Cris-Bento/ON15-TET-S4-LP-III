8a9db9b117181181999e1a3278d94b741114e7a0
Cris-Bento <bentocristiane1@gmail.com>
2022-04-22 21:50:06 -0300

    Resolução do desafio da semana 4

--- desafio-loja.js
+++ desafio-loja.js
@@ -11,6 +11,24 @@ let clienteLilit = [
     {produto: 'Tênis casual preto', valor: 120.0}, 
     {produto: 'meia calça transparente', valor: 30.0}]
 
+let clienteCris = [
+    {produto: 'Vestido preto', valor: 279.0},
+    {produto: 'Jaqueta de couro', valor: 350.0},
+    {produto: 'Camiseta branca', valor: 49.0},
+    {produto: 'Blaser forrado off white', valor: 199.0},
+    {produto: 'Scarpin nude 37', valor: 125.0}]
+
+let clienteSilvana = [
+    {produto: 'Jaqueta sarja branca', valor: 145.0},
+    {produto: 'Camiseta vermelha', valor: 34.0},
+    {produto: 'Regata estampada', valor: 29.0},
+    {produto: 'Calça jeans', valor: 98.0},
+    {produto: 'Tênis All Star 37', valor: 289.0},
+    {produto: 'Sandália salto médio 37', valor: 78.0},
+    {produto: 'Bolsa tiracolo caramelo', valor: 120.0},
+    {produto: 'Meias de lã branca', valor: 15.0}]    
+            
+
 let valorTotal = [] 
 let descontoAplicado = [] 
 
@@ -26,11 +44,11 @@ function calcularDesconto(valor) {
     } 
 }
 
-function realizarCompra(comprasDaCliente) {
+function carrinhoDeCompras(produtosDaCliente) {
 
-    let quantidadeDeProdutos = comprasDaCliente.length
+    let quantidadeDeItens = produtosDaCliente.length
 
-    comprasDaCliente.forEach(item => {
+    produtosDaCliente.forEach(item => {
         valorTotal.push(item.valor)
         calcularDesconto(item.valor)
     })
@@ -40,7 +58,7 @@ function realizarCompra(comprasDaCliente) {
     let valorFinal = valorTotal - descontoAplicado
     let dataDaCompra = new Date()
 
-    let notaFiscal = {
+    let cupomFiscal = {
         "Valor Total": `R$${valorTotal.toFixed(2).replace('.', ',')}`,
         "Desconto Aplicado": `R$${descontoAplicado.toFixed(2).replace('.', ',')}`,
         "Valor Final": `R$${valorFinal.toFixed(2).replace('.', ',')}`,
@@ -51,14 +69,16 @@ function realizarCompra(comprasDaCliente) {
         })
     }
 
-    if(quantidadeDeProdutos > 10 || valorFinal > 800.0) {
+    if(quantidadeDeItens > 10 || valorFinal > 800.0) {
         return console.table({
-            ...notaFiscal,
-            "Bônus": 'Você ganhou um cupom de R$50,00 para a sua prõxima compra'
+            ...cupomFiscal,
+            "Bônus": 'Parabéns! Você ganhou um voucher de desconto no valor de R$50,00 para a sua próxima compra!'
         })
     } else {
         return console.table(notaFiscal)
     }
 }
 
-realizarCompra(clienteLilit)
+carrinhoDeCompras(clienteLilit)
+carrinhoDeCompras(clienteCris)
+carrinhoDeCompras(clienteSilvana)
